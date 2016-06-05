282f11b0eb2ea58418c2c2784881119eb188bfea
SynysterLove <synlgp@gmail.com>
2016-06-05 19:29:13 -0300

    Pasa el test de factorizar el 121

--- factores-primos-3/spec/factorizador_spec.rb
+++ factores-primos-3/spec/factorizador_spec.rb
@@ -3,10 +3,17 @@ require_relative '../model/factorizador'
 
 describe 'Factorizador' do
 
-  it 'should return 2 2 5 when buscar_menor_divisor 20' do
+  it 'should return 2 2 5 when factorizar 20' do
 
     factorizador = Factorizador.new
     expect(factorizador.factorizar 20).to eq [2,2,5]
+
+  end
+
+  it 'should return 11 11 when factorizar 121' do
+
+    factorizador = Factorizador.new
+    expect(factorizador.factorizar 121).to eq [11,11]
 
   end
 
