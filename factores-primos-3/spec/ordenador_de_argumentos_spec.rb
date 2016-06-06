9e16e743f9ad533a48375c39c4daa8ccef2d46b5
SynysterLove <synlgp@gmail.com>
2016-06-06 09:58:09 -0300

    Pasa el test de colocar pretty en primera posicion si no hay formato

--- factores-primos-3/spec/ordenador_de_argumentos_spec.rb
+++ factores-primos-3/spec/ordenador_de_argumentos_spec.rb
@@ -4,10 +4,19 @@ require_relative '../model/ordenador_de_argumentos'
 
 describe 'OrdenadorDeArgumentos' do
 
-  it 'que ponga el pretty en la primera posicion, estando especificado el formato' do
+  it 'que ponga el formato en la primera posicion, estando especificado el formato' do
 
     ordenador = OrdenadorDeArgumentos.new
     argumentos = ['alguna_cosa', 'alguna_otra', '--format=pretty']
+    ordenador.ordenar argumentos
+    expect(argumentos).to eq ['--format=pretty', 'alguna_otra', 'alguna_cosa']
+
+  end
+
+  it 'que ponga el pretty en la primera posicion, cuando no hay formato' do
+
+    ordenador = OrdenadorDeArgumentos.new
+    argumentos = ['alguna_cosa', 'alguna_otra']
     ordenador.ordenar argumentos
     expect(argumentos).to eq ['--format=pretty', 'alguna_otra', 'alguna_cosa']
 
